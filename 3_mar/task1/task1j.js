394265f95a387667e21ee24fc54025132091d375
nancy <nancyverma632002@gmail.com>
2023-03-03 17:44:49 +0530

    final comit

--- 3_mar/task1/task1j.js
+++ 3_mar/task1/task1j.js
@@ -1,11 +1,11 @@
-
-
 function customerName() {
     var memName = document.getElementById("membersName").value;
+    return memName;
 }
 
 const customerMembers = () => {
     var mem = document.getElementById("members").value;
+    return mem;
 }
 
 var timeslot = document.getElementById("timeSlot");
@@ -14,30 +14,54 @@ var tableslot = document.getElementById("tableSlot");
 let tablesBooked = [];
 function newBooking(time) {
 
+    for (let k = 0; k < tableslot.length; k++) {
+        tableslot.options[k].disabled = false;
+    }
     for (let i = 0; i < tablesBooked.length; i++) {
         if (tablesBooked[i].Time === time.value) {
             for (let j = 0; j < tableslot.length; j++) {
                 if (tableslot.options[j].value === tablesBooked[i].bookedTable) {
                     tableslot.options[j].disabled = true;
-                    //alert("Sorry this table is not available for this time slot!!!");
+                }
+                else {
+                    tableslot.options[j].disabled = false;
                 }
             }
         }
     }
 }
-
+let nAME = customerName();
 function confirmBooking() {
     const selectedTimeSlot = timeslot.options[timeslot.selectedIndex].value;
     const selectedTableSlot = tableslot.options[tableslot.selectedIndex].value;
 
-    const obj = { Time: selectedTimeSlot, bookedTable: selectedTableSlot };
-    tablesBooked.push(obj);
-    localStorage.setItem("slot",obj);
+    var bookId = Math.floor(Math.random() * (700 - 60 + 1)) + 60;
 
-    let a=localStorage.getItem('slot');
+    const obj = { Time: selectedTimeSlot, bookedTable: selectedTableSlot, bookID: bookId };
+    tablesBooked.push(obj);
+    console.log(tablesBooked);
+    localStorage.setItem(nAME, JSON.stringify(obj));
+    let a = JSON.parse(localStorage.getItem(nAME));
     console.log(a);
 
-    //document.getElementById("details").innerHTML=`${memName} your ${selectedTableSlot} is booked for tome-slot ${selectedTimeSlot}`;
+    timeslot.selectedIndex = 0;
+    tableslot.selectedIndex = 0;
 
+    document.getElementById("details").innerHTML = "WELCOME!!!" + customerName() + " your table " + selectedTableSlot + " is booked for timings " + selectedTimeSlot + " AND YOUR BOOKING ID IS:" + bookId;
+
+    for (let i = 0; i < timeslot.length; i++) {              //enable all the options in both lists when printing is done
+        timeslot.options[i].disabled = false;
+        tableslot.options[i].disabled = false;
+    }
+}
+
+function cancelBooking() {
+    let cancelid = document.getElementById("cancelID").value;
+    for (let i in tablesBooked) {
+        if (tablesBooked[i].bookID === cancelid) {
+            tablesBooked.splice(i, 1);
+            localStorage.removeItem(nAME);
+        }
+    }
 }
 
