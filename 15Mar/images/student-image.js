1f2453cf64726bdee3a2dbaafa85268f3ef1bfcf
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-03-15 21:59:49 +0530

    Update student-image.js

--- 15Mar/images/student-image.js
+++ 15Mar/images/student-image.js
@@ -1,10 +1,10 @@
-//using multer as a middleware 
+//using multer as a middleware used for uploading files
 const multer = require("multer");
 
 // //storage for image
 const Storage = multer.diskStorage({
     destination: (req, file, cb) => {  //cb callback
-        cb(null, "uploads") //tells the multer to create a file upload and store the data(image) inside it
+        cb(null, "uploads") //tells the multer to create a file upload and store the data(image) inside it,upload the photo to the server in a folder called `uploads`
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now());  //Date.now is used to give unique identity to every image otherwise it will fetch first image everytime
