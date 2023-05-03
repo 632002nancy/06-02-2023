05d2042259bec6e297fb3663da28d8fd99a0b3d8
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-05-03 20:41:48 +0530

    completed
    completed

---
+++ 15Mar/model/student-model.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+const studentSchema=mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+    },
+    rollNo:{
+        type:Number,
+        require:true,
+    },
+    class:{
+        type:String,
+        require:true,
+    },
+    subjects:{
+        type:Array,
+        require:true,
+    },
+    image:{
+        data:String,  
+        contentType:String,
+    }
+});
+
+module.exports=mongoose.model("studentData",studentSchema);
+
+ 
