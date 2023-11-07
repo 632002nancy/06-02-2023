9a24287f66a5edc848ade6e6256009f01685e65c
nancyverma632002@gmail.com <124571076+632002nancy@users.noreply.github.com>
2023-11-07 11:56:25 +0530

    Update student-routes.js
    a

--- 16_mar/routes/student-routes.js
+++ 16_mar/routes/student-routes.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const {addStudent,displayStudent, updateStudent, deleteStudent}=require("../controller/student-controller.js")
+const {addStudent,displayStudent, updateStudent}=require("../controller/student-controller.js")
 const upload=require("../multer/student-image.js")
 const router=express.Router();
 
 router.post("/create",upload.single("image"),addStudent);  //create
 router.get("/display",displayStudent);  //read
 router.post("/update",upload.single("image"),updateStudent);  //update
-router.post("/delete",deleteStudent);  //delete
+// router.post("/delete",deleteStudent);  //delete
 
 module.exports=router;
